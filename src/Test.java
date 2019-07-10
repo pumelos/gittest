1cc1f87189e3b9fe81dc6b3a17fa175ea796ee45
pumelos <52732745+pumelos@users.noreply.github.com>
2019-07-10 20:41:59 +0800

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,10 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
-        System.out.println(4);
-        System.out.println(5);
+        System.out.println(11111);
+        System.out.println(22222);
+        System.out.println(33333);
+        System.out.println(44444);
+        System.out.println(55555);
     }
 }
